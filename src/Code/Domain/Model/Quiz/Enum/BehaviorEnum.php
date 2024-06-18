2d9b2005bf3241abb5da557445f1f70d25c02083
Cristiano Pacheco <chris.spb25@gmail.com>
2024-06-18 11:59:28 -0300

    Changes the behavior enum value exclude all products

--- src/Code/Domain/Model/Quiz/Enum/BehaviorEnum.php
+++ src/Code/Domain/Model/Quiz/Enum/BehaviorEnum.php
@@ -7,7 +7,7 @@ namespace App\Code\Domain\Model\Quiz\Enum;
 enum BehaviorEnum: string
 {
     case NONE = 'none';
-    case EXCLUDE_PRODUCTS = 'exclude_products';
+    case EXCLUDE_ALL_PRODUCTS = 'exclude_all_products';
     case ASK_QUESTION = 'ask_question';
     case RECOMMEND_PRODUCTS = 'recommend_products';
 }
--- tests/Unit/Code/Domain/Model/Quiz/Enum/BehaviorEnumTest.php
+++ tests/Unit/Code/Domain/Model/Quiz/Enum/BehaviorEnumTest.php
@@ -12,7 +12,7 @@ final class BehaviorEnumTest extends TestCase
     public function testContract(): void
     {
         $this->assertEquals('none', BehaviorEnum::NONE->value);
-        $this->assertEquals('exclude_products', BehaviorEnum::EXCLUDE_PRODUCTS->value);
+        $this->assertEquals('exclude_all_products', BehaviorEnum::EXCLUDE_ALL_PRODUCTS->value);
         $this->assertEquals('ask_question', BehaviorEnum::ASK_QUESTION->value);
         $this->assertEquals('recommend_products', BehaviorEnum::RECOMMEND_PRODUCTS->value);
     }
