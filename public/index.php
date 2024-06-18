cdf3569954a9f8e450db93dd40d206acf35f04cf
Cristiano Pacheco <chris.spb25@gmail.com>
2024-06-18 12:18:32 -0300

    Updates the symfony config to read the files from the infrastructure folder

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Kernel;
+declare(strict_types=1);
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
+use App\Code\Infrastructure\Symfony\Kernel;
+
+return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
